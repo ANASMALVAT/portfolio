9ccef246d15b6314cd6288a3433585170c66c299
Anas Malvat <68893726+ANASMALVAT@users.noreply.github.com>
2024-01-07 02:34:56 -0400

    Update education.tsx

--- components/education.tsx
+++ components/education.tsx
@@ -20,7 +20,7 @@ export default function Education() {
           <React.Fragment key={index}>
             <VerticalTimelineElement
                contentStyle={{
-                background:"#E2DCDF",
+                background:"#FDFAFB",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
@@ -35,7 +35,7 @@ export default function Education() {
               date={item.date}
               icon={item.icon}
               iconStyle={{
-                background:"#E2DCDF",
+                background:"#FDFAFB",
                 fontSize: "1.5rem",
                 color:"#0f172a",
                 
