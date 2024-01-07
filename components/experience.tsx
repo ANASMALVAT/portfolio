b8b6ad0f0b918eccf7e0507911cd61cbe2f23aea
Anas Malvat <68893726+ANASMALVAT@users.noreply.github.com>
2024-01-07 02:29:13 -0400

    Update experience.tsx

--- components/experience.tsx
+++ components/experience.tsx
@@ -20,7 +20,7 @@ export default function Experience() {
           <React.Fragment key={index}>
             <VerticalTimelineElement
               contentStyle={{
-                background:"#cbd5e1",
+                background:"#E2DCDF",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
                 textAlign: "left",
@@ -35,7 +35,7 @@ export default function Experience() {
               date={item.date}
               icon={item.icon}
               iconStyle={{
-                background:"#cbd5e1",
+                background:"#E2DCDF",
                 fontSize: "1.5rem",
                 color:"#0f172a",
                 
